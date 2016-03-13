ece2e773261bfea432576ec99d4f4e441853476e
masterUNG <phrombutr@gmail.com>
2016-03-13 10:13:31 +0700

    Create Adpater

---
+++ app/src/main/java/appewtc/masterung/mytraffic/MyAdapter.java
@@ -0,0 +1,13 @@
+package appewtc.masterung.mytraffic;
+
+import android.content.Context;
+
+/**
+ * Created by masterUNG on 3/13/16 AD.
+ */
+public class MyAdapter {
+
+    //Explicit
+    private Context context;
+
+}   // Main Class
